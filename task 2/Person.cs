1df7447d27998f317203ce2b5d48278aa588591c
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-09-06 12:40:50 +0300

    Create class Person, add few exception

---
+++ task 2/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Person
+    {
+        private string name;
+        private string surname;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                try
+                {
+                    if (char.IsLower(value[0]))
+                    {
+                        throw new ArgumentException("Expected upper case letter! Argument : Name");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    if (value.Length < 4)
+                    {
+                        throw new ArgumentException("Expected length at least 4 symbols! Argument : Name");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+
+                }
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                try
+                {
+                    if (char.IsLower(value[0]))
+                    {
+                        throw new ArgumentException("Expected upper case letter! Argument : Name");
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+                try
+                {
+                    if (value.Length < 3) throw new ArgumentException("Expected upper case letter! Argument: Surname");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using task_2;
+
+class Task
+{
+    static void Main()
+    {
+        Person a = new Person();
+        a.Name = "Ab s";
+        Console.WriteLine(a.Name);
+
+    }
+    public static void line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
